18fbbb775609afbdc6624a991c871ef656cc291c
raviranjan mahto <rvrnjnmahto@gmail.com>
2022-11-20 01:48:01 +0530

    custome api bad req api and not found api added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,21 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
+
   if (!name || !email || !password) {
-    throw new Error("Please provide all values!");
+    throw new BadRequestError("Please provide all values!");
+  }
+
+  const userAlreadyExist = await User.findOne({ email });
+  if (userAlreadyExist) {
+    throw new BadRequestError("Email already register, Please login!");
   }
 
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.OK).json({ user });
 };
 const login = async (req, res) => {
   res.send("login user");
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,9 +1,9 @@
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err.message);
+  console.log(err);
   const defaultError = {
-    StatusCodes: StatusCodes.INTERNAL_SERVER_ERROR,
+    StatusCodes: err.statusCodes || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong, try again later!",
   };
   if (err.name === "ValidationError") {
