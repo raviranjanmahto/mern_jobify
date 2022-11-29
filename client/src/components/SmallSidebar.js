b576b62064d623694154013bab33d215f422d8aa
raviranjan mahto <rvrnjnmahto@gmail.com>
2022-11-29 12:28:32 +0530

    ig sidebar added

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -5,6 +5,7 @@ import Wrapper from "../assets/wrappers/SmallSidebar";
 import { useAppContext } from "../context/appContext";
 import links from "../utils/links";
 import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
   const { showSidebar, toggleSidebar } = useAppContext();
@@ -22,24 +23,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className='nav-links'>
-            {links.map(link => {
-              const { text, path, id, icon } = link;
-              return (
-                <NavLink
-                  to={path}
-                  key={id}
-                  onClick={toggleSidebar}
-                  className={({ isActive }) =>
-                    isActive ? "nav-link active" : "nav-link"
-                  }
-                >
-                  <span className='icon'>{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks toggleSidebar={toggleSidebar} />
         </div>
       </div>
     </Wrapper>
