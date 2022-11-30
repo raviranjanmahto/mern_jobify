e01e6dcd2a6afa1cbd075380bc4e25344c557003
raviranjan mahto <rvrnjnmahto@gmail.com>
2022-11-30 18:40:29 +0530

    add job backend

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,16 @@
-const createJob = (req, res) => {
-  res.send("create job");
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, UnAuthenticateError } from "../errors/index.js";
+
+const createJob = async (req, res) => {
+  const { position, company } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError("Please provide all values!");
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 const deleteJob = (req, res) => {
   res.send("deleteJob job");
