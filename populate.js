f32f309860f456669513f29d226f3e779e0cb328
raviranjan mahto <rvrnjnmahto@gmail.com>
2022-12-13 19:50:52 +0530

    dummy db data added n logic in populate.js

---
+++ populate.js
@@ -0,0 +1,23 @@
+import { readFile } from "fs/promises";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDb from "./db/connect.js";
+import Job from "./models/Job.js";
+
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URL);
+    await Job.deleteMany();
+    const jsonProducts = JSON.parse(
+      await readFile(new URL("./mock-data.json", import.meta.url))
+    );
+    await Job.create(jsonProducts);
+    console.log("Success!!!");
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start();
