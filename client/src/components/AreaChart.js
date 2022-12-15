770226c1c7169f8eaafb4e09dfd173a8602cb49c
raviranjan mahto <rvrnjnmahto@gmail.com>
2022-12-15 12:33:09 +0530

    moment for date and recahrts and barchart added

---
+++ client/src/components/AreaChart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AreaChart = () => {
+  return (
+    <div>
+      <h1>AreaChart</h1>
+    </div>
+  );
+};
+
+export default AreaChart;
--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Wrapper from "../assets/wrappers/ChartsContainer";
+import { useAppContext } from "../context/appContext";
+import AreaChart from "./AreaChart";
+import BarChart from "./BarChart";
 
 const ChartsContainer = () => {
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
   return (
-    <div>
-      <h1>chart container</h1>
-    </div>
+    <Wrapper>
+      <h4>Monthly Application</h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
   );
 };
 
