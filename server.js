9f97493ad75266009ab72b07f57c6f196d91d1c5
raviranjan mahto <rvrnjnmahto@gmail.com>
2022-12-16 16:11:53 +0530

    security pakages added helmet xssClean mongosanatize

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
 
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+
 // import cors from "cors";
 import notFoundMiddleware from "./middleware/notFound.js";
 import errorHandlerMiddleware from "./middleware/errorHandler.js";
@@ -27,12 +31,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.use(express.json());
-// app.get("/", (req, res) => {
-//   res.json({ msg: "hello" });
-// });
-app.get("/api/v1", (req, res) => {
-  res.json({ msg: "hello" });
-});
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
