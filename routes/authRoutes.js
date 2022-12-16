47175fbf4c6c21f9afc453dc0a00d55c25b30372
raviranjan mahto <rvrnjnmahto@gmail.com>
2022-12-16 16:21:34 +0530

    api rate limiter added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,9 +3,17 @@ import { register, login, updateUser } from "../controllers/authController.js";
 import authenticateUser from "../middleware/auth.js";
 
 const router = express.Router();
+import rateLimiter from "express-rate-limit";
 
-router.route("/register").post(register);
-router.route("/login").post(login);
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, //15 minutes
+  max: 10,
+  message:
+    "To many request from this IP address. Please try again after 15 minutes!",
+});
+
+router.route("/register").post(apiLimiter, register);
+router.route("/login").post(apiLimiter, login);
 router.route("/updateUser").patch(authenticateUser, updateUser);
 
 export default router;
